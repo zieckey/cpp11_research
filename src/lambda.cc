8e7299d789234f150df91212c1582be0769c9dfe
weizili.360.laptop <zieckey@gmail.com>
2014-11-27 16:33:06 +0800

    Add lambda_test2

--- src/lambda.cc
+++ src/lambda.cc
@@ -26,3 +26,36 @@ TEST_UNIT(lambda_test1)
     std::function<int(int)> func2 = [](int i) { return i + 4; };
     std::cout << "func2: " << func2(6) << '\n';
 }
+
+
+
+TEST_UNIT(lambda_test2)
+{
+    using namespace std;
+
+    // Create a vector object that contains 10 elements.
+    vector<int> v;
+    for (int i = 0; i < 10; ++i) {
+        v.push_back(i);
+    }
+
+    // Count the number of even numbers in the vector by 
+    // using the for_each function and a lambda.   这里相当于创建了一个函数对象，构造函数的参数为（&evenCount）
+    int evenCount = 0;
+    for_each(v.begin(), v.end(), [&evenCount](int n) {
+        cout << n;
+
+        if (n % 2 == 0) {
+            cout << " is even " << endl;
+            ++evenCount;
+        }
+        else {
+            cout << " is odd " << endl;
+        }
+    });
+
+    // Print the count of even numbers to the console.
+    cout << "There are " << evenCount
+        << " even numbers in the vector." << endl;
+}
+
