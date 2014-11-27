564cccb04317e003a4f74a435c4ccfb479d9a604
weizili.360.laptop <zieckey@gmail.com>
2014-11-27 12:06:03 +0800

    add future test.

---
+++ src/future.cc
@@ -0,0 +1,29 @@
+#include "test_common.h"
+
+#include <iostream>
+#include <future>
+#include <thread>
+
+TEST_UNIT(future_test)
+{
+    // future from a packaged_task
+    std::packaged_task<int()> task([](){ std::cout << "tid=" << std::this_thread::get_id() << "\n";  return 7; }); // wrap the function
+    std::future<int> f1 = task.get_future();  // get a future
+    std::thread(std::move(task)).detach(); // launch on a thread
+
+    // future from an async()
+    std::future<int> f2 = std::async(std::launch::async, [](){ std::cout << "tid=" << std::this_thread::get_id() << "\n"; return 8; });
+
+    // future from a promise
+//     std::promise<int> p;
+//     std::future<int> f3 = p.get_future();
+//     std::thread([](std::promise<int> p){ p.set_value_at_thread_exit(9); },
+//         std::move(p)).detach();
+
+    std::cout << "tid=" << std::this_thread::get_id() << "  " << "Waiting... " << std::flush;
+    f1.wait();
+    f2.wait();
+    //f3.wait();
+    std::cout << "Done!\nResults are: "
+        << f1.get() << ' ' << f2.get() << ' ' << /*f3.get() <<*/ '\n';
+}
