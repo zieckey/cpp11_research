9fc07594beae9e6ac930de4a9a7f11438eadbbda
weizili.360.laptop <zieckey@gmail.com>
2014-11-28 10:42:06 +0800

    add math-log test

--- src/math.cc
+++ src/math.cc
@@ -8,3 +8,14 @@ TEST_UNIT(pow_test)
     H_TEST_ASSERT(std::pow(5, 2) == 25); //5的2次幂
     H_TEST_ASSERT(std::pow(5, 3) == 125); //5的3次幂
 }
+
+
+TEST_UNIT(log_test)
+{
+    //自然对数的底数e是由一个重要极限给出的.我们定义：当x趋于无限时，lim(1+1/x)^x=e. 
+    //e是一个无限不循环小数，其值约等于2.718281828
+    //Computes the the natural (base e) logarithm of arg.
+    double f1 = std::log(2);
+    //f1 = 0.69314718055994529
+    H_TEST_ASSERT(f1 == 0.69314718055994529);
+}
