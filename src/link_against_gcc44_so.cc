bb19fa25299980682e6ee6b94e012a585da74ebb
weizili.build17 <zieckey@gmail.com>
2015-05-28 10:32:25 +0800

    add GCC VERSION test code

--- src/link_against_gcc44_so.cc
+++ src/link_against_gcc44_so.cc
@@ -1,5 +1,6 @@
 #include "test_common.h"
 
+#include <osl/include/exp.h>
 #include <wdurlsplit/include/url_parser.h>
 
 TEST_UNIT(urlparser_test)
@@ -8,4 +9,21 @@ TEST_UNIT(urlparser_test)
     wdurl::UrlParser p(url);
     std::cout << "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii host=" << p.host << "\n";
     H_TEST_ASSERT(p.host == "www.haosou.com");
+}
+
+#define H_COMPILER_VERSION (((__GNUC__)*100) + \
+                           (__GNUC_MINOR__*10) + \
+                           __GNUC_PATCHLEVEL__)
+
+void p()
+{
+    std::cout << "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy gcc version=" << H_COMPILER_VERSION << "\n";
+}
+
+
+TEST_UNIT(GCC_VERSION)
+{
+    std::cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx gcc version=" << H_COMPILER_VERSION << "\n";
+    std::function< void () > f(p);
+    f();
 }
