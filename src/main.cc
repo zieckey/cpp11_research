4a96d163bcce6c294a7501ee4d3825294d1ef739
weizili.build9 <zieckey@gmail.com>
2016-12-30 18:31:23 +0800

    Add gtest code

--- src/main.cc
+++ src/main.cc
@@ -1,17 +1,6 @@
 
 #include "test_common.h"
 
-
-#ifdef WIN32
-    #ifdef _DEBUG
-        #pragma comment(lib, "gtestd.lib")
-        #pragma comment(lib, "gtest_maind.lib")
-    #else
-        #pragma comment(lib, "gtest.lib")
-        #pragma comment(lib, "gtest_main.lib")
-    #endif
-#endif
-
 namespace
 {
 #ifdef WIN32
